dce2feecf457544bca15a491b38d548fbc54994d
TimLedger <tim_ledger@mail.ru>
2023-12-27 20:37:27 +0600

    обновлен компонент App.tsx, инициализирован state с несколькими начальными задачами

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import AddTaskForm from './AddTaskForm';
+import Task from './Task';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface TaskItem {
+  id: string;
+  text: string;
 }
 
-export default App
+const App: React.FC = () => {
+  const [tasks, setTasks] = useState<TaskItem[]>([
+    { id: '1', text: 'Купить подарки' },
+    { id: '2', text: 'Нарядить елку' },
+    { id: '2', text: 'Испечь угощение' },
+  ]);
+
+  const handleAddTask = (text: string) => {
+    const newTask: TaskItem = {
+      id: new Date().toISOString(),  
+      text,
+    };
+    setTasks([...tasks, newTask]);
+  };
+
+  const handleDeleteTask = (id: string) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  };
+
+  return (
+    <div>
+      <AddTaskForm onAddTask={handleAddTask} />
+      {tasks.map(task => (
+        <Task key={task.id} id={task.id} text={task.text} onDeleteTask={handleDeleteTask} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
